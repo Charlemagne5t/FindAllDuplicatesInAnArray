c012f7dfa46eaceb08c87d6d558c7c5928224264
alek <wait82985@gmail.com>
2023-06-14 12:35:45 +0600

    initial commit. 3 test cases

---
+++ src/main/java/Solution.java
@@ -0,0 +1,8 @@
+import java.util.List;
+
+public class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        //TODO
+        return null;
+    }
+}
---
+++ src/test/java/SolutionTest.java
@@ -0,0 +1,28 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SolutionTest {
+    @Test
+    public void findDuplicatesTest1() {
+        int[] nums = {4, 3, 2, 7, 8, 2, 3, 1};
+        List<Integer> output = new ArrayList<>(List.of(2, 3));
+        Assert.assertEquals(output, new Solution().findDuplicates(nums));
+    }
+
+    @Test
+    public void findDuplicatesTest2() {
+        int[] nums = {1, 1, 2};
+        List<Integer> output = new ArrayList<>(List.of(1));
+        Assert.assertEquals(output, new Solution().findDuplicates(nums));
+    }
+
+    @Test
+    public void findDuplicatesTest3() {
+        int[] nums = {1};
+        List<Integer> output = new ArrayList<>();
+        Assert.assertEquals(output, new Solution().findDuplicates(nums));
+    }
+}
